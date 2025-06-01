e10ae292f41a76b61489aa74e48603092041d109
Aneesh Shukla <aneeshshukla.tech@gmail.com>
2025-06-01 19:34:54 +0530

    Revert "added tackle animation"
    This reverts commit b866f6883eab758ab1094ec1cde3699281fe6df3.

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -1,10 +1,7 @@
 class_name Player
 extends CharacterBody2D
 
-const DURATION_TACKLE = 200
-
 enum ControlScheme {CPU, P1, P2}
-enum State {MOVING, TACKLING}
 
 @export var control_scheme: ControlScheme
 @export var speed: float
@@ -13,25 +10,13 @@ enum State {MOVING, TACKLING}
 @onready var player_sprite: Sprite2D = $player_sprite
 
 var heading := Vector2.RIGHT
-var state := State.MOVING
-var time_start_tackle := Time.get_ticks_msec()
-
 
 func _process(_delta: float) -> void:
 	if control_scheme == ControlScheme.CPU:
 		pass    # for AI movement
 	else:
-		if state == State.MOVING:
-			handle_human_movement()
-			set_movement_animation()
-			if velocity.x != 0 and KeyUtils.is_action_just_pressed(control_scheme, KeyUtils.Action.SHOOT):
-				state = State.TACKLING
-				time_start_tackle = Time.get_ticks_msec()
-		elif state == State.TACKLING:
-			animation_player.play('tackle')
-			if Time.get_ticks_msec() - time_start_tackle > DURATION_TACKLE:
-				state = State.MOVING
-	
+		handle_human_movement()
+	set_movement_animation()
 	set_heading()
 	flip_sprites()
 	move_and_slide()
