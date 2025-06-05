12e457ea34433e479fd5c602823b192eb676a439
Aneesh Shukla <aneeshshukla.tech@gmail.com>
2025-06-05 12:32:23 +0530

    added carried state with oscillation
    error fixing:
    ball_state.gd: line 4
    -    signal  state_tansition_requested(new_state: BallState)
    +    signal  state_tansition_requested(new_state: Ball.State)

--- scripts/ball_states/ball_state.gd
+++ scripts/ball_states/ball_state.gd
@@ -1,13 +1,15 @@
 class_name BallState
 extends Node
 
-signal  state_tansition_requested(new_state: BallState)
+signal  state_tansition_requested(new_state: Ball.State)
 
 var ball : Ball = null
 var carrier : Player = null
 var player_detection_area : Area2D = null
+var animation_player : AnimationPlayer = null
 
-func setup(context_ball: Ball, context_player_detection_area: Area2D, context_carrier: Player) -> void:
+func setup(context_ball: Ball, context_player_detection_area: Area2D, context_carrier: Player, context_animation_player: AnimationPlayer) -> void:
 	ball = context_ball
 	player_detection_area = context_player_detection_area
 	carrier = context_carrier
+	animation_player = context_animation_player
--- scripts/ball_states/ball_state_carried.gd
+++ scripts/ball_states/ball_state_carried.gd
@@ -1,8 +1,25 @@
 class_name BallStateCarried
 extends BallState
 
+const OFFSET_FROM_PLAYER := Vector2(10,4)
+const DRIBBLE_FREAQUENCY := 10.0
+const DRIBBLE_INTENSITY := 2.5
+
+var dribble_time := 0.0
+
 func _enter_tree() -> void:
 	assert(carrier != null)
 
-func _process(_delta: float) -> void:
-	ball.position = carrier.position
+func _process(delta: float) -> void:
+	var vx := 0.0
+	dribble_time += delta
+	if carrier.velocity != Vector2.ZERO:
+		if carrier.velocity.x != 0.0:
+			vx = cos(dribble_time * DRIBBLE_FREAQUENCY) * DRIBBLE_INTENSITY
+		else:
+			vx = 0.0
+		ball.animation_player.speed_scale = carrier.heading.x
+		ball.animation_player.play('roll')
+	else:
+		ball.animation_player.pause()
+	ball.position = carrier.position + Vector2(vx + carrier.heading.x * OFFSET_FROM_PLAYER.x, OFFSET_FROM_PLAYER.y)
